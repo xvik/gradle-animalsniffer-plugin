d0a7ae03fdb4be2c3e3f0f62375c14ad374b8b3b
Vyacheslav Rusakov <vyarus@gmail.com>
2017-07-17 18:13:47 +0700

    add info task test for multiple signatures

--- src/test/groovy/ru/vyarus/gradle/plugin/animalsniffer/info/SignatureInfoKitTest.groovy
+++ src/test/groovy/ru/vyarus/gradle/plugin/animalsniffer/info/SignatureInfoKitTest.groovy
@@ -74,4 +74,47 @@ class SignatureInfoKitTest extends AbstractKitTest {
         result.output.contains("Signature animalsnifferCacheMain.sig (1.3 Mb) contains 14007 classes")
         result.output.contains("com.sun                        7115")
     }
+
+
+    def "Check multiple signatures of cache task output"() {
+        setup:
+        build """
+            plugins {
+                id 'java'
+                id 'ru.vyarus.animalsniffer'
+            }
+            
+            animalsniffer {
+                cache.enabled = true
+            }
+
+            repositories { mavenCentral()}
+            dependencies {
+                signature 'org.codehaus.mojo.signature:java16-sun:1.0@signature'
+                signature 'net.sf.androidscents.signature:android-api-level-14:4.0_r4@signature'                
+                compile 'org.slf4j:slf4j-api:1.7.25'
+            }
+            
+            task printSignature(type: ru.vyarus.gradle.plugin.animalsniffer.info.SignatureInfoTask) {
+                signature = animalsnifferCacheMain.outputs.files
+                depth = 2
+            } 
+
+        """
+//        debug()
+
+        when: "run task"
+        BuildResult result = run('printSignature')
+
+        then: "task successful"
+        result.task(':printSignature').outcome == TaskOutcome.SUCCESS
+
+        then: "sun signature read"
+        result.output.contains("Signature animalsnifferCacheMain_!java16-sun-1.0.sig (1.3 Mb) contains 13710 classes")
+        result.output.contains("com.sun                        7115")
+
+        then: "android signature read"
+        result.output.contains("Signature animalsnifferCacheMain_!android-api-level-14-4.0_r4.sig (317.1 Kb) contains 3167 classes")
+        result.output.contains("org.apache                     367")
+    }
 }
