984eed2c4c887f6c5abc04295fe79671a9cc5f5f
Vyacheslav Rusakov <vyarus@gmail.com>
2022-08-16 12:11:45 +0700

    update upstream test

--- src/test/groovy/ru/vyarus/gradle/plugin/animalsniffer/UpstreamKitTest.groovy
+++ src/test/groovy/ru/vyarus/gradle/plugin/animalsniffer/UpstreamKitTest.groovy
@@ -9,7 +9,7 @@ import org.gradle.testkit.runner.TaskOutcome
  */
 class UpstreamKitTest extends AbstractKitTest {
 
-    public static final String GRADLE_VERSION = '7.2'
+    public static final String GRADLE_VERSION = '7.5.1'
 
     def "Check violation detection without cache task"() {
         setup:
